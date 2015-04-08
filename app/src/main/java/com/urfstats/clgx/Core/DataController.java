7a0a71116bb7efc77c9818b1266d13d675018bdb
Pablo J <pablo0910@users.noreply.github.com>
2015-04-08 18:24:16 +0200

    New way to save data added

---
+++ app/src/main/java/com/urfstats/clgx/Core/DataController.java
@@ -0,0 +1,69 @@
+package com.urfstats.clgx.Core;
+
+import com.urfstats.clgx.LoLData.Game;
+import com.urfstats.clgx.LoLData.URFStatistics;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class DataController {
+
+    public final static String FILENAME = "urfdata.bin";
+    private ArrayList<Game> games;
+    private URFStatistics stats;
+    private String filesDir;
+
+    public DataController(String dir) {
+
+        filesDir = dir;
+        loadData();
+
+    }
+
+    public void saveData() {
+
+        File file = new File(filesDir+FILENAME);
+        ObjectOutputStream oos;
+        try {
+
+            oos = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+            oos.writeObject(games);
+            oos.writeObject(stats);
+            oos.close();
+
+        } catch(Exception e) {
+
+            System.err.println("Error! Data not saved! " +e);
+
+        }
+
+    }
+
+    public void loadData() {
+
+        File file = new File(filesDir+FILENAME);
+        ObjectInputStream ois;
+        try {
+
+            ois = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+            games = (ArrayList<Game>) ois.readObject();
+            stats = (URFStatistics) ois.readObject();
+            ois.close();
+
+        } catch(Exception e) {
+
+            System.err.println("Error! Data not loaded! " +e);
+            games = new ArrayList<>();
+            stats = new URFStatistics(games);
+
+        }
+
+    }
+
+}
