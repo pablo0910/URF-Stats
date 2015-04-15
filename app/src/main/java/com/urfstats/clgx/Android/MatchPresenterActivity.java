2874221be99f3081cdc3b57a505ebc937156e5c9
Pablo J <pablo0910@users.noreply.github.com>
2015-04-15 16:45:36 +0200

    New Activity which shows the match whose stats the user wants to watch

---
+++ app/src/main/java/com/urfstats/clgx/Android/MatchPresenterActivity.java
@@ -0,0 +1,54 @@
+package com.urfstats.clgx.Android;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.urfstats.clgx.LoLData.Game;
+import com.urfstats.clgx.R;
+
+import java.util.Date;
+
+public class MatchPresenterActivity extends ActionBarActivity {
+
+    MatchPresenterFragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.matchpresenter_activity);
+        Bundle dateContainer = getIntent().getExtras();
+
+        final Toolbar actionBar = (Toolbar) findViewById(R.id.actionBarUpButton);
+        setSupportActionBar(actionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fragment = MatchPresenterFragment.newInstance((Game) dateContainer.getSerializable("game"));
+        getSupportFragmentManager().beginTransaction().replace(R.id.containerStatsListActivity, fragment).commit();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_useless, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/urfstats/clgx/Android/MatchPresenterFragment.java
@@ -0,0 +1,45 @@
+package com.urfstats.clgx.Android;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.urfstats.clgx.LoLData.Game;
+import com.urfstats.clgx.R;
+
+public class MatchPresenterFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "game";
+
+    private Game match;
+
+    public static MatchPresenterFragment newInstance(Game match) {
+        MatchPresenterFragment fragment = new MatchPresenterFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_PARAM1, match);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MatchPresenterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            match = (Game) getArguments().getSerializable(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.matchpresenter_fragment, container, false);
+    }
+
+}
